6b19dea22fbaf1e3cf017ad4198d423b5f5be54c
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2025-11-30 00:34:27 -0500

    typing effects anims!!!

--- brandon-oa-demo/obstacle.py
+++ brandon-oa-demo/obstacle.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, RED
+import math
+
 
 class Obstacle:
     def __init__(self):
@@ -11,9 +13,24 @@ class Obstacle:
         self.y = random.randint(0, SCREEN_HEIGHT - self.height)
         self.speed = random.randint(3, 7)
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.DIAM = 50
+        self.CHARGE = -1
+        self.MASS = 10
+        
+    
 
     def move(self):
         self.rect.y += self.speed
+        
+    def distanceSquared(self, player):
+        dx = self.rect.centerx - player.rect.centerx
+        dy = self.rect.centery - player.rect.centery
+        return dx * dx + dy * dy
+
+    def distance(self, player):
+        return math.hypot(self.rect.centerx - player.rect.centerx, self.rect.centery - player.rect.centery)
+
+        
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, self.rect.center, self.width // 2)
--- brandon-oa-demo/player.py
+++ brandon-oa-demo/player.py
@@ -1,9 +1,10 @@
 import pygame
 import math
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, BLUE
+import random
 
 class Player:
-    def __init__(self):
+    def __init__(self, obstacles, dt, m, fMax):
         self.width = 50
         self.height = 50
         self.color = BLUE
@@ -12,6 +13,12 @@ class Player:
         self.speed = 5
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.target_index = 0
+        self.virtual_force = False
+        self.obstacles = obstacles
+        self.dt = dt
+        self.m = m
+        self.fMax = fMax
+
 
     def follow_course(self, course, course_is_closed=False):
         if not course:
@@ -38,6 +45,25 @@ class Player:
             angle = math.atan2(dy, dx)
             self.rect.x += self.speed * math.cos(angle)
             self.rect.y += self.speed * math.sin(angle)
+            
+    def update_position(self):
+        dirX = 0
+        minS = 200
+        
+        
+            
+    def range(self, obstacle, range):
+        dist = obstacle.distanceSq(self)
+        if dist < range:
+            return True
+        else:
+            return False
+
+    def range(self, x, mean, stddev):
+        rand = random.random()
+        noise = stddev * rand.gauss(0, 1) + mean
+        return x + noise
+        
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, self.rect.center, self.width // 2)
